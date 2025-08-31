6e4521d72b960e2513c581b846ef930fa0eb8402
Computhink-CS128 <cs128@Computhink.com.sg>
2025-08-31 03:59:54 +0000

    31/8

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,20 @@
+//I typed "0" by acident on the tittle its supposed to be "9"
 
+let x = 600
+let y = 400
+let diameter = 50
+function setup(){
+ createCanvas(x, y);
+ background(220);
+}
+
+function draw() {
+fill("black");
+stroke("red");
+strokeWeight(5);
+
+// triangle(x/2+diameter/2, y/2+diameter/2, x/2+diameter/2,  y/2-diameter/2, x/2-diameter/2,  y/2+diameter/2); 
+triangle( x/2+diameter/2,  y/2-diameter/2, x/2-diameter/2,  y/2+diameter/2,x/2+diameter/2, y/2+diameter/2); 
+circle(x/2 - 100, y/2, diameter); 
+rect(x/2 +100, y/2- diameter/2, diameter, diameter);
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,18 @@
+let x = 600
+let y = 400
+let diameter = 30
+
+function setup(){
+ createCanvas(x, y);
+ background(220);
+}
+
+function draw() {
+for (let i = 1; i < 11; i++) {
+fill(20*i,0,0);
+circle(diameter*i, diameter*i, diameter); 
+}
+}
+
+
 
